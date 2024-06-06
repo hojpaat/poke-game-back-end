7440ac04d8a40c019e4d4e876787c0aaf16ff4d9
hojpaat <judit.of.horvath@gmail.com>
2024-06-06 13:55:28 +0100

    update config

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,14 @@ import { pageRouter } from './router/page.router';
 import { DEFAULT_PORT } from './constants/enviroment';
 
 const app = express();
+const PORT = process.env.PORT || DEFAULT_PORT;
 
 app.use(bodyParser.json( { limit: '50mb' }));
 app.use(cors());
 
 app.use("/", pageRouter)
 
-app.listen(process.env.PORT || DEFAULT_PORT , () => {
+app.listen( PORT , () => {
     console.log("Server is listening on port: " + 8080);
 });
 
